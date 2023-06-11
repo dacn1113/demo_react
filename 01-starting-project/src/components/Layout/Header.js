1eda9f5d11033ce0f63f0a9a94f701f388b8ff4b
An <dacn1113@gmail.com>
2023-06-11 17:20:13 +0700

    Add Meal Items & Displaying & fix bug section

--- 01-starting-project/src/components/Layout/Header.js
+++ 01-starting-project/src/components/Layout/Header.js
@@ -2,14 +2,14 @@ import { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
-import HeaderCartButtonn from "./HeaderCardButton";
+import HeaderCartButton from "./HeaderCardButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButtonn />
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicous food!" />
--- 01-starting-project/src/components/Meals/AvailableMeals.js
+++ 01-starting-project/src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -27,10 +29,19 @@ const DUMMY_MEALS = [
   },
 ];
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
-    <section className={classes.meal}>
-      <ul>{mealsList}</ul>
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
