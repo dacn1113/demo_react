cad1351c82ccb59ea5f53289d4d4ddda193c5d1c
An <dacn1113@gmail.com>
2023-06-12 16:41:48 +0700

    Hide & close Cart

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,14 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- 01-starting-project/src/components/Layout/HeaderCardButton.js
+++ 01-starting-project/src/components/Layout/HeaderCardButton.js
@@ -3,7 +3,7 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButtonn = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
