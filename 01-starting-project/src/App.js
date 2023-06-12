842210cd10701c5a98bbfb1d26297eaae19dcb8c
An <dacn1113@gmail.com>
2023-06-12 23:59:37 +0700

    Fix err reduce

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -3,7 +3,7 @@ import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/cartProvider";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShow, setCartIsShow] = useState(false);
--- 01-starting-project/src/components/Layout/HeaderCardButton.js
+++ 01-starting-project/src/components/Layout/HeaderCardButton.js
@@ -1,16 +1,24 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
-const HeaderCartButtonn = (props) => {
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 };
 
-export default HeaderCartButtonn;
+export default HeaderCartButton;
