de951038a25b819f9b4a008d294850a18212aedf
An <dacn1113@gmail.com>
2023-06-15 12:20:35 +0700

    Add remove item

--- 01-starting-project/src/components/Cart/Cart.js
+++ 01-starting-project/src/components/Cart/Cart.js
@@ -10,8 +10,12 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
